3af4f4e3a9d02c3b298d17748e957665cefc3688
Wiley Corning <Wiley.Corning@ncf.edu>
2015-12-02 20:35:02 -0500

    Replaced empty database.py with project 1 version

--- modules/Server/database.py
+++ modules/Server/database.py
@@ -7,6 +7,8 @@
 # Copyright 2012 Linkoping University
 # -----------------------------------------------------------------------------
 
+# Wiley Corning 8/31/15
+
 """Implementation of a simple database class."""
 
 import random
@@ -20,21 +22,23 @@ class Database(object):
         self.db_file = db_file
         self.rand = random.Random()
         self.rand.seed()
-        #
-        # Your code here.
-        #
-        pass
-
+        
+        db = open(self.db_file,'r')
+        contents = db.read()
+        db.close()
+        self.fortunes = str.split(contents,'\n%\n')
+        self.fortunes=self.fortunes[:-1] # Remove empty fortune at end
+        
     def read(self):
         """Read a random location in the database."""
-        #
-        # Your code here.
-        #
-        pass
+        return(self.fortunes[self.rand.randint(0,len(self.fortunes)-1)])
 
     def write(self, fortune):
         """Write a new fortune to the database."""
-        #
-        # Your code here.
-        #
-        pass
+        # Add to stored list
+        self.fortunes.append(fortune)
+        
+        # Append to file
+        db = open(self.db_file,'a')
+        db.write(fortune+'\n%\n')
+        db.close()
