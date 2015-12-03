5f78a9e57d725dbf91ade23b4a11fdd96d5e6fef
sarah-russell12 <sarah.russell12@ncf.edu>
2015-12-02 21:07:36 -0500

    Added require_object() to interface of NameServer.  Returns the reference to a particular server of a particular type for debugging purposes.
    Signed-off-by: sarah-russell12 <sarah.russell12@ncf.edu>

--- lab5/name_server.py
+++ lab5/name_server.py
@@ -46,7 +46,7 @@ class NameServer(object):
     # [obj0]            | [ (0, addr0), (3, addr3) ]
     # [obj1]            | [ (1, addr1), (4, addr4), (5, addr5) ]
     # [obj2]            | [ (2, addr2) ]
-    
+
     def __init__(self):
         self.lock = ReadWriteLock()
         self.peers = dict()         # Contains a set of peers for each object type
@@ -57,16 +57,16 @@ class NameServer(object):
 
     def register(self, obj_type, address):
         self._check_all_alive(obj_type) # Make sure everyone in our group is still alive
-        
+
         address = tuple(address)    # The address might come in as a list.
         logging.debug("NameServer registering peer at {}".format(address))
-        
+
         # We're making modifications to the NameServer's data
         self.lock.write_acquire()
         obj_hash = address       # Set the hash to the address (for now)
         obj_id = self.next_id
         self.next_id += 1
-        t = (obj_id, obj_hash) 
+        t = (obj_id, obj_hash)
         self.lock.write_release()
 
         # We're adding the address to the group
@@ -74,13 +74,13 @@ class NameServer(object):
         self.lock.write_acquire() # Acquire the lock before we add ourselves to the group
         group.add(t)              # Add ourselves
         self.lock.write_release() # Release the lock
-        
+
         logging.info("NameServer done registering peer at {}".format(address))
         return t
 
     def unregister(self, obj_id, obj_type, obj_hash):
         logging.debug("NameServer unregistering peer at {}".format(tuple(obj_hash)))
-        
+
         # Get the data necessary
         group = self._get_group(obj_type)
         t = (obj_id, tuple(obj_hash))
@@ -100,30 +100,34 @@ class NameServer(object):
         # This is very obnoxious.
         self._check_all_alive(obj_type) # Make sure everyone in our group is still alive
         return "null"
-    
+
     def get_peers(self, obj_type):
         return list(self._get_group(obj_type))
-    
+
+    def require_object(self, server_type, server_id):
+        server_type_list = self.get_peers(server_type)
+        return server_type_list[server_id]
+        
     def _get_group(self, obj_type):
         # Create our group if it doesn't already exist
         self.lock.write_acquire()
         if obj_type not in self.peers.keys():
             self.peers[obj_type] = set()
         self.lock.write_release()
-        
+
         # Fetch and return the group
         self.lock.read_acquire()
         group = self.peers.get(obj_type)
         self.lock.read_release()
         return group
-    
+
     def _check_all_alive(self, obj_type):
         logging.info("NameServer confirming connections to all peers" \
                  + " of type {}.".format(obj_type))
         group = copy.deepcopy(self._get_group(obj_type))
         for peer in group:
             self._check_alive(obj_type, peer)
-    
+
     def _check_alive(self, obj_type, peer):
         logging.debug("NameServer confirming connection to peer {}.".format(peer[0]))
         if not self._is_alive(obj_type, peer, 5):
@@ -133,7 +137,7 @@ class NameServer(object):
             logging.info("Removing peer {}.".format(t))
             group.remove(t)
             self.lock.write_release()
-    
+
     def _is_alive(self, obj_type, peer, timeout=5):
         try:
             parent_conn, child_conn = multiprocessing.Pipe(duplex=False)
@@ -186,15 +190,15 @@ class NameServer(object):
 
 if __name__ == "__main__":
     logging.info("NameServer listening to: {}:{}".format(server_address[0], server_address[1]))
-    
+
     nameserver = NameServer()
-    
+
     listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listener.bind(("", server_address[1]))
     listener.listen(1)
-    
+
     logging.info("Press Ctrl-C to stop the name server...")
-    
+
     try:
         while True:
             conn, addr = listener.accept()
